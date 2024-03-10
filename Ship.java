25cc544ec3e82419d676ba04c7883f0b9c5076c1
Kurkomisu <AndreiChubenko@proton.me>
2024-03-11 00:25:58 +0100

    Changed the constructor of class ship

--- Ship.java
+++ Ship.java
@@ -39,13 +39,12 @@ public class Ship {
     public Coordinate[] getCoordinates(){
         return this.shipCoordinates;
     }
-    Ship(int xn, int yn){
+    Ship(){
         System.out.print("Put the Coordinates for the bow of your battleship in the following format: A 1 H B.\n");
         System.out.print("H for vertical, V for Horizontal\n");
         System.out.print("B for 4-squares ship, C for 3-squares ship , D for 2-squares ship, T for 1-square ship\n");
         String input = scanner.nextLine();
         coordintes = coordinateConverter(input.split(" ")[0].charAt(0),Integer.parseInt(input.split(" ")[1]));
-
         if (input.split(" ")[2].toLowerCase() == "h" || input.split(" ")[2].toLowerCase() == "v" ){
             this.is_horizontal = input.split(" ")[2] == "H";
         }
@@ -60,13 +59,13 @@ public class Ship {
         else if (input.split(" ")[3].toLowerCase() == "c"){
             this.shipType = ShipType.CRUISER;
             this.shipCoordinates = new Coordinate[3];
-            if (new Coordinate(Coordinate.State.SHIP,xn,yn).coordinateCheck()){
+            if (new Coordinate(Coordinate.State.SHIP,coordintes[0],coordintes[1]).coordinateCheck()){
                 for(int i = 0; i < this.getLength();i++){
                     if (! is_horizontal) {
-                        shipCoordinates[i] = new Coordinate(Coordinate.State.SHIP, xn, yn+i);
+                        shipCoordinates[i] = new Coordinate(Coordinate.State.SHIP, coordintes[0], coordintes[1]+i);
                     }
                     else{
-                        shipCoordinates[i] = new Coordinate(Coordinate.State.SHIP, xn+i, yn);
+                        shipCoordinates[i] = new Coordinate(Coordinate.State.SHIP, coordintes[0]+i, coordintes[1]);
                     }
                 }
             }
@@ -76,14 +75,14 @@ public class Ship {
         }
         else if (input.split(" ")[3].toLowerCase() == "d"){
             this.shipCoordinates = new Coordinate[2];
-            if (new Coordinate(Coordinate.State.SHIP,xn,yn).coordinateCheck()){
-                shipCoordinates[0] = new Coordinate(Coordinate.State.SHIP,xn,yn);
+            if (new Coordinate(Coordinate.State.SHIP,coordintes[0],coordintes[1]).coordinateCheck()){
+                shipCoordinates[0] = new Coordinate(Coordinate.State.SHIP,coordintes[0],coordintes[1]);
                 for(int i = 0; i < this.getLength();i++){
                     if (! is_horizontal) {
-                        shipCoordinates[1] = new Coordinate(Coordinate.State.SHIP, xn, yn+i);
+                        shipCoordinates[1] = new Coordinate(Coordinate.State.SHIP, coordintes[0], coordintes[1]+i);
                     }
                     else{
-                        shipCoordinates[1] = new Coordinate(Coordinate.State.SHIP, xn+i, yn);
+                        shipCoordinates[1] = new Coordinate(Coordinate.State.SHIP, coordintes[0]+i, coordintes[1]);
                     }
                     }
 
@@ -95,8 +94,8 @@ public class Ship {
         }
         else if (input.split(" ")[3].toLowerCase() == "t"){
             this.shipCoordinates = new Coordinate[1];
-            if (new Coordinate(Coordinate.State.SHIP,xn,yn).coordinateCheck()){
-                shipCoordinates[0] = new Coordinate(Coordinate.State.SHIP,xn,yn);
+            if (new Coordinate(Coordinate.State.SHIP,coordintes[0],coordintes[1]).coordinateCheck()){
+                shipCoordinates[0] = new Coordinate(Coordinate.State.SHIP,coordintes[0],coordintes[1]);
             }
             else{
                 System.out.print("Ship position is not valid.");
